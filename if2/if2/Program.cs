4d652a5f3efd7ccdd2261cbbfea56f936bc8bdb2
onashka03 <148713028+onashka03@users.noreply.github.com>
2023-10-23 14:03:22 +0500

    Add files via upload

---
+++ if2/if2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace if2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = 5; 
+
+            if (number > 0)
+            {
+                number += 1;
+            }
+            else
+            {
+                number -= 2;
+            }
+
+            Console.WriteLine(number);
+            Console.ReadKey();
+        }
+    }
+}
