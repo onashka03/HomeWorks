12f07ec1f3b9f04df8510ce11797e08dba964aee
onashka03 <148713028+onashka03@users.noreply.github.com>
2023-11-13 15:18:03 +0500

    Add files via upload

---
+++ Boolean5/Boolean5/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Boolean5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int A = 10;
+            int B = 20;
+
+            bool statement = A >= 0 || B < -2;
+
+            Console.WriteLine("Истинность высказывания: " + statement);
+
+            Console.ReadKey();
+
+        }
+    }
+}
