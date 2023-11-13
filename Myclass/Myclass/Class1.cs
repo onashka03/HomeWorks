1b258a19869ce57159086d4c01cd916aa6b8daa6
onashka03 <148713028+onashka03@users.noreply.github.com>
2023-11-13 15:30:59 +0500

    Add files via upload

---
+++ Myclass/Myclass/Class1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Myclass
+{
+    public class Myclass
+    {
+        public int[] num = { 23, 25, 41, 36 };
+        public int number1 = 32;
+        public int number2 = 100;
+        public string name = "Aziza";
+
+        public void For()
+    {
+        for (int i = 1; i <= 10; i++)
+        {
+            num[2] = 1;
+            Console.WriteLine(num[2]);
+        }
+    }
+
+        public void Foreach()
+        {
+            foreach (char name in "Aziza")
+            {
+                Console.WriteLine(name);           
+            }
+            foreach(int num1 in num)
+            {
+                Console.Write(num + " ");
+            }
+        }
+
+    }
+}
---
+++ Myclass/Myclass/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Myclass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Myclass myclass1 = new Myclass();
+            myclass1.For();
+            Myclass myclass2= new Myclass();
+            myclass2.Foreach();
+            Console.ReadKey();
+
+        }
+    }
+}
