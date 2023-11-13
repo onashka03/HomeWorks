cf3723b49b1f38e08385bff37a016500d994d33d
onashka03 <148713028+onashka03@users.noreply.github.com>
2023-11-13 15:13:04 +0500

    Add files via upload

---
+++ Boolean3/Boolean3/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Boolean3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int A = 10; 
+
+if (A % 2 == 0)
+{
+    Console.WriteLine("Число A является четным");
+}
+else
+{
+    Console.WriteLine("Число A не является четным");
+}
+
+                Console.ReadKey();
+            }
+        }
+    }
