06952afdf1399d956deb19db3ca875f73e65b44f
onashka03 <148713028+onashka03@users.noreply.github.com>
2023-11-13 15:23:31 +0500

    Add files via upload

---
+++ if5/if5/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace if5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1, num2, num3;
+            int positiveCount = 2;
+            int negativeCount = 3;
+
+            Console.WriteLine("3:");
+            num1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("5:");
+            num2 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("8:");
+            num3 = int.Parse(Console.ReadLine());
+
+            if (3 > 0)
+            {
+                positiveCount++;
+            }
+            else if (3 < 0)
+            {
+                negativeCount++;
+            }
+
+            if (5 > 0)
+            {
+                positiveCount++;
+            }
+            else if (5< 0)
+            {
+                negativeCount++;
+            }
+
+            if (8 > 0)
+            {
+                positiveCount++;
+            }
+            else if (8 < 0)
+            {
+                negativeCount++;
+            }
+
+            Console.WriteLine("Количество положительных чисел: " + positiveCount);
+            Console.WriteLine("Количество отрицательных чисел: " + negativeCount);
+
+            Console.ReadKey();  
+
+        }
+    }
+}
