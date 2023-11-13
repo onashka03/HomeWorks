989f4498e8830d9bb59bd5de3360363ecbdcbd67
onashka03 <148713028+onashka03@users.noreply.github.com>
2023-11-13 15:28:26 +0500

    Add files via upload

---
+++ Case2/Case2/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Case2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int K = 3;
+
+        string result;
+
+        switch (K)
+        {
+            case 1:
+                result = "плохо";
+                break;
+            case 2:
+                result = "неудовлетворительно";
+                break;
+            case 3:
+                result = "удовлетворительно";
+                break;
+            case 4:
+                result = "хорошо";
+                break;
+            case 5:
+                result = "отлично";
+                break;
+            default:
+                result = "ошибка";
+                break;
+        }
+
+        Console.WriteLine(result);
+        Console.ReadKey();
+        }
+    }
+}
