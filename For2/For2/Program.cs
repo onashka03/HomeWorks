ebed7c966b89e769ce9bb02b9eb4d84ad5d439d5
onashka03 <148713028+onashka03@users.noreply.github.com>
2023-11-13 15:20:22 +0500

    Add files via upload

---
+++ For2/For2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace For2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int A = 3;
+            int B = 8;
+
+            int N = B - A + 1;
+
+            Console.WriteLine("Числа между {0} и {1}:", A, B);
+            for (int i = A; i <= B; i++)
+            {
+                Console.WriteLine(i);
+            }
+
+            Console.WriteLine("Количество чисел: " + N);
+        Console.ReadKey();
+        }
+    }
+}
