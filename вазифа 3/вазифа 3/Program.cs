44291dd68c69184e1c1a7ae25a25f7a7860efe7b
onashka03 <148713028+onashka03@users.noreply.github.com>
2023-11-13 15:31:55 +0500

    Add files via upload

---
+++ вазифа 3/вазифа 3/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace вазифа_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            library[] readers = new library[2];
+        
+        readers[0] = new library("Азиза ", 12345, "Физика", new DateTime(1990, 5, 15), "123-456-7890");
+        readers[1] = new library("Аниса ", 54321, "Математика", new DateTime(1985, 10, 20), "987-654-3210");
+
+        readers[0].takeBook(3);
+        readers[0].takeBook("Приключения", "Словарь", "Энциклопедия");
+        readers[0].takeBook(new Book("Приключения", "Автор1"), new Book("Словарь", "Автор2"), new Book("Энциклопедия", "Автор3"));
+
+        readers[1].returnBook(2);
+        readers[1].returnBook("Роман", "Поэма");
+        readers[1].returnBook(new Book("Роман", "Автор4"), new Book("Поэма", "Автор5"));
+        Console.ReadKey();
+    }
+        }
+    }
---
+++ вазифа 3/вазифа 3/library.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace вазифа_3
+{
+    class library
+    {
+        public string fullName;
+        public int readerNumber;
+        public string faculty;
+        public DateTime dateOfBirth;
+        public string phoneNumber;
+
+        public library(string fullName, int readerNumber, string faculty, DateTime dateOfBirth, string phoneNumber)
+        {
+            this.fullName = fullName;
+            this.readerNumber = readerNumber;
+            this.faculty = faculty;
+            this.dateOfBirth = dateOfBirth;
+            this.phoneNumber = phoneNumber;
+        }
+
+        public void takeBook(int numberOfBooks)
+        {
+            Console.WriteLine(this.fullName + " взял " + numberOfBooks + " книги");
+        }
+
+        public void takeBook(params string[] bookNames)
+        {
+            Console.Write(this.fullName + " взял книги: ");
+            for (int i = 0; i < bookNames.Length; i++)
+            {
+                Console.Write(bookNames[i]);
+                if (i < bookNames.Length - 1)
+                {
+                    Console.Write(", ");
+                }
+            }
+            Console.WriteLine();
+        }
+
+        public void takeBook(params Book[] books)
+        {
+            Console.Write(this.fullName + " взял книги: ");
+            for (int i = 0; i < books.Length; i++)
+            {
+                Console.Write(books[i].Title);
+                if (i < books.Length - 1)
+                {
+                    Console.Write(", ");
+                }
+            }
+            Console.WriteLine();
+        }
+
+        public void returnBook(int numberOfBooks)
+        {
+            Console.WriteLine(this.fullName + " вернул " + numberOfBooks + " книги");
+        }
+
+        public void returnBook(params string[] bookNames)
+        {
+            Console.Write(this.fullName + " вернул книги: ");
+            for (int i = 0; i < bookNames.Length; i++)
+            {
+                Console.Write(bookNames[i]);
+                if (i < bookNames.Length - 1)
+                {
+                    Console.Write(", ");
+                }
+            }
+            Console.WriteLine();
+        }
+
+        public void returnBook(params Book[] books)
+        {
+            Console.Write(this.fullName + " вернул книги: ");
+            for (int i = 0; i < books.Length; i++)
+            {
+                Console.Write(books[i].Title);
+                if (i < books.Length - 1)
+                {
+                    Console.Write(", ");
+                }
+            }
+            Console.WriteLine();
+        }
+    }
+
+  public  class Book
+    {
+        public string Title { get; set; }
+        public string Author { get; set; }
+
+        public Book(string title, string author)
+        {
+            Title = title;
+            Author = author;
+        }
+    }
+}
