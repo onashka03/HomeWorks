d76f9fb2d7bb3061b7cd952e58007809bfd719cc
onashka03 <148713028+onashka03@users.noreply.github.com>
2023-11-13 15:10:17 +0500

    Add files via upload

---
+++ integer4/integer2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace integer2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int M = 70000;
+            int kg = 1000;
+            int result = 0;
+
+
+           
+
+
+            {
+                result = 7000 / 1000;
+                Console.WriteLine(result);
+                Console.ReadKey();
+
+            
+            
+            
+            
+            }
+           
+
+
+        }
+    }
+}
